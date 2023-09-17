0ad8aa7d907b1543e9e06b11e2e8a3ee3b178134
HamiD <clashroyale13501350@gmail.com>
2023-09-17 15:28:53 +0330

    posts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,15 @@ module.exports = {
       width:{
         312: "312px",
         72: "72px",
+        470: "470px"
       },
       spacing:{
-        'three': "3px"
+        'three': "3px",
+        'twoHalf': "2.8px"
       },
       screens:{
-        670: "670px"
+        670: "670px",
+        480: "485px"
       }
     },
   },
